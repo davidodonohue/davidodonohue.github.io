b35a3aae9e5fd377bad552b169d1739116d20cbb
unknown <david.odonohue@korddefence.com.au>
2020-11-07 19:38:01 +1100

    commit

--- components/SavedFood.js
+++ components/SavedFood.js
@@ -12,11 +12,11 @@ const SavedFood = Vue.component('saved-food', {
     template: `<div>
 <p style="flex-grow:2">{{ data.name }}</br>
 {{ Math.round(data.proteinratio) + '%'}} of calories from protein</br>
-{{this.data.protein}} grams of protein per 100g</br>
-{{'$' + parseFloat(this.data.costOfProtein).toFixed(2)}} per 100g of protein</br>
+{{data.protein}} grams of protein per 100g</br>
+{{'$' + parseFloat(data.costOfProtein).toFixed(2)}} per 100g of protein</br>
 {{ Math.round(data.calories) }} calories per 100g</br>
-{{parseFloat(this.data.proteinPerItem).toFixed(2)}} grams of protein per item</br>
-{{Math.round(this.data.caloriesPerItem)}} calories per item</br>
+{{parseFloat(data.proteinPerItem).toFixed(2)}} grams of protein per item</br>
+{{Math.round(data.caloriesPerItem)}} calories per item</br>
 <button @click="remove" class='remove'><i class="fas fa-ban"></i> Remove</button>
 <button @click="edit" class="edit"><i class="fas fa-edit"></i> Edit</button>
 <hr v-if="hr" style="width: 100%;">
