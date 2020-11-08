9f218790a3af86e8fab5f0a99502fa4d7c469b92
unknown <david.odonohue@korddefence.com.au>
2020-11-09 08:53:39 +1100

    commit

--- components/SavedFood.js
+++ components/SavedFood.js
@@ -10,13 +10,13 @@ const SavedFood = Vue.component('saved-food', {
         }
     },
     template: `<div>
-<p style="flex-grow:2">{{ data.name }}</br>
-{{ Math.round(data.proteinratio) + '%'}} of calories from protein</br>
-{{data.protein}} grams of protein per 100g</br>
-{{'$' + parseFloat(data.costOfProtein).toFixed(2)}} per 100g of protein</br>
-{{ Math.round(data.calories) }} calories per 100g</br>
-{{parseFloat(data.proteinPerItem).toFixed(2)}} grams of protein per item</br>
-{{Math.round(data.caloriesPerItem)}} calories per item</br>
+<p>{{ data.name }}</br>
+<span v-if="data.proteinratio">{{ Math.round(data.proteinratio) + '%'}} of calories from protein</br></span>
+<span v-if="data.protein">{{data.protein}} grams of protein per 100g</br></span>
+<span v-if="data.costOfProtein">{{'$' + parseFloat(data.costOfProtein).toFixed(2)}} per 100g of protein</br></span>
+<span v-if="data.calories">{{ Math.round(data.calories) }} calories per 100g</br></span>
+<span v-if="data.proteinPerItem">{{parseFloat(data.proteinPerItem).toFixed(2)}} grams of protein per item</br></span>
+<span v-if="data.caloriesPerItem">{{Math.round(data.caloriesPerItem)}} calories per item</br></span>
 <button @click="remove" class='remove'><i class="fas fa-ban"></i> Remove</button>
 <button @click="edit" class="edit"><i class="fas fa-edit"></i> Edit</button>
 <hr v-if="hr" style="width: 100%;">
