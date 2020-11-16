eae4d763c8c2631b966a30ccf37bfa1494f8a7df
David O'Donohue <51901087+davidodonohue@users.noreply.github.com>
2020-11-16 19:18:22 +1100

    Update SavedFood.js

--- components/SavedFood.js
+++ components/SavedFood.js
@@ -23,13 +23,12 @@ const SavedFood = Vue.component('saved-food', {
     template: `<div>
 <p>{{ data.name }}</br>
 <span v-if="data.proteinPercent">{{ Math.round(data.proteinPercent) + '%'}} of calories from protein</br></span>
-<span v-if="data.proteinPer100g">{{data.proteinPer100g}} grams of protein per 100g</br></span>
 <span v-if="data.costPerProtein">{{'$' + parseFloat(data.costPerProtein).toFixed(2)}} per 100g of protein</br></span>
-<span v-if="data.caloriesPer100g">{{ Math.round(data.caloriesPer100g) }} calories per 100g</br></span>
-<span v-if="data.proteinPerItem">{{parseFloat(data.proteinPerItem).toFixed(2)}} grams of protein per item</br></span>
-<span v-if="data.caloriesPerItem">{{Math.round(data.caloriesPerItem)}} calories per item</br></span>
+<span v-if="data.proteinPerServe">{{parseFloat(data.proteinPerServe).toFixed(2)}} grams of protein per serve</br></span>
+<span v-if="data.caloriesPerServe">{{Math.round(data.caloriesPerServe)}} calories per serve</br></span>
+<span v-if="data.serveGrams">{{Math.round(data.serveGrams)}} grams per serve</br></span>
 <button @click="remove" class='remove'><i class="fas fa-ban"></i> Remove</button>
-<button @click="edit" class="edit"><i class="fas fa-edit"></i> Edit</button>
+<button v-if="data.editEnabled" @click="edit" class="edit"><i class="fas fa-edit"></i> Edit</button>
 <input type="number" v-model="mixGrams" placeholder="Grams to add" />
 <input type="number" v-model="mixServes" placeholder="Serves to add" />
 <button @click="mix" class="mix"><i class="fas fa-utensils"></i> Add to mixer</button>
