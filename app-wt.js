c3a4796faf38c1448a4387ad2f2b7f330cb159f6
David O'Donohue <51901087+davidodonohue@users.noreply.github.com>
2023-04-07 21:32:13 +1000

    Update app-wt.js

--- app-wt.js
+++ app-wt.js
@@ -1,21 +1,15 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const weightInput = document.getElementById('weight');
-const dateInput = document.getElementById('date');
-const addDataButton = document.getElementById('addData');
-const clearDataButton = document.getElementById('clearData');
-const anchorDateInput = document.getElementById('anchorDate');
-const percentageChangeSelect = document.getElementById('percentageChange');
-const projectionLengthSelect = document.getElementById('projectionLength');
-const updateProjectionButton = document.getElementById('updateProjection');
+  const weightInput = document.getElementById('weight');
+  const dateInput = document.getElementById('date');
+  const addDataButton = document.getElementById('addData');
+  const clearDataButton = document.getElementById('clearData');
+  const anchorDateInput = document.getElementById('anchorDate');
+  const projectionLengthSelect = document.getElementById('projectionLength');
 
-// Set the current date as the default value for date input
-dateInput.valueAsDate = new Date();
+  // Set the current date as the default value for date input
+  dateInput.valueAsDate = new Date();
 
-let weightData = (JSON.parse(localStorage.getItem('weightData')) || [
-    { x: new Date().getTime() - 86400000 * 7, y: 150 },
-    { x: new Date().getTime() - 86400000 * 6, y: 151 },
-    { x: new Date().getTime() - 86400000 * 5, y: 152 },
-  ]).map((dataPoint) => ({
+  let weightData = (JSON.parse(localStorage.getItem('weightData')) || []).map((dataPoint) => ({
     x: new Date(dataPoint.x).getTime(),
     y: dataPoint.y,
   }));
@@ -32,7 +26,10 @@ const chart = new ApexCharts(document.querySelector('#weightChart'), {
     },
     series: [
       { name: 'Weight', data: weightData.map((point) => ({ x: point.x, y: point.y })) },
-      { name: 'Projected Weight', data: [] },
+      { name: '-1% Projected Weight', data: [] },
+      { name: '-0.5% Projected Weight', data: [] },
+      { name: '+0.5% Projected Weight', data: [] },
+      { name: '+1% Projected Weight', data: [] },
     ],
     xaxis: {
       type: 'datetime',
@@ -54,8 +51,16 @@ const chart = new ApexCharts(document.querySelector('#weightChart'), {
       curve: 'smooth',
     },
   });
+
+    // Set the anchor date from local storage or use the current date
+    const storedAnchorDate = localStorage.getItem('anchorDate');
+    anchorDateInput.value = storedAnchorDate ? storedAnchorDate : new Date().toISOString().slice(0, 10);  
+  
   
   chart.render();
+    // Update the Y-axis min and max values and show the projected data after rendering the chart
+    updateYAxisMinMax();
+    calculateProjectedWeight();
 
   addDataButton.addEventListener('click', () => {
     const weight = parseFloat(weightInput.value);
@@ -131,12 +136,14 @@ clearDataButton.addEventListener('click', () => {
   // Calculate the projected weight based on the anchor date and percentage change
   function calculateProjectedWeight() {
     const anchorDate = anchorDateInput.value;
-    const percentageChangePerWeek = parseFloat(percentageChangeSelect.value);
     const projectionLength = parseInt(projectionLengthSelect.value);
   
     if (!anchorDate) {
       return;
     }
+
+    const percentages = [-0.01, -0.005, 0.005, 0.01];
+    const projectedData = percentages.map(() => []);
   
     const anchorDateObj = new Date(anchorDate);
     const anchorTimestamp = anchorDateObj.getTime();
@@ -148,26 +155,55 @@ clearDataButton.addEventListener('click', () => {
   }
 
   const avgWeight = lastSevenDaysData.reduce((sum, dataPoint) => sum + dataPoint.y, 0) / lastSevenDaysData.length;
-  const projectedData = [];
 
   for (let i = 1; i <= projectionLength; i++) {
     const projectedDate = new Date(anchorDateObj);
     projectedDate.setDate(projectedDate.getDate() + i);
-    const projectedWeight = avgWeight * Math.pow(1 + percentageChangePerWeek / 7, i);
-    projectedData.push({ x: projectedDate.getTime(), y: projectedWeight });
+
+    percentages.forEach((percentage, index) => {
+      const projectedWeight = avgWeight * Math.pow(1 + percentage / 7, i);
+      projectedData[index].push({ x: projectedDate.getTime(), y: projectedWeight });
+    });
   }
 
   chart.updateSeries([
     { name: 'Weight', data: weightData.map((point) => ({ x: point.x, y: point.y })) },
-    { name: 'Projected Weight', data: projectedData },
+    { name: '-1% Projected Weight', data: projectedData[0] },
+    { name: '-0.5% Projected Weight', data: projectedData[1] },
+    { name: '+0.5% Projected Weight', data: projectedData[2] },
+    { name: '+1% Projected Weight', data: projectedData[3] },
   ]);
 
   updateYAxisMinMax();
 }
 
-// Update the projection when the user clicks the "Update Projection" button
-updateProjectionButton.addEventListener('click', calculateProjectedWeight);
+  // Update the graph length when the user changes the "Graph Length" selection
+  projectionLengthSelect.addEventListener('change', () => {
+    const graphLength = parseInt(projectionLengthSelect.value);
+    const now = new Date();
+    const startTime = now.getTime() - graphLength * 86400000 / 2;
 
-// Initial update of the projected weight
-calculateProjectedWeight();
+    chart.updateOptions({
+      xaxis: {
+        min: startTime,
+        max: startTime + graphLength * 86400000,
+      },
+    });
+
+    // Update the projection when the graph length changes
+    calculateProjectedWeight();
+  });
+
+  // Update the projection when the anchor date changes
+  anchorDateInput.addEventListener('change', calculateProjectedWeight);
+
+  // Initial update of the projected weight and graph length
+  calculateProjectedWeight();
+  projectionLengthSelect.dispatchEvent(new Event('change'));
+
+  // Update the anchor date in local storage when it changes
+  anchorDateInput.addEventListener('change', () => {
+    localStorage.setItem('anchorDate', anchorDateInput.value);
+    calculateProjectedWeight();
+  });
 });
